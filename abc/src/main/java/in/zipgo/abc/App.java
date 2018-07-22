33ae7599c27f29c206f2610263a19ab0880880f2
prashant <prashantranjan816@gmail.com>
2018-07-22 13:31:59 +0530

    Commit1

---
+++ abc/src/main/java/in/zipgo/abc/App.java
@@ -0,0 +1,15 @@
+package in.zipgo.abc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Hi");
+    }
+}
